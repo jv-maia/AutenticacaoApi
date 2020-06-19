71d585fbc39fec3ab11820b8ed26d9d33b2689b6
jv-maia <55468329+jv-maia@users.noreply.github.com>
2020-06-19 18:33:57 -0300

    Initial commit

---
+++ AutenticacaoApi/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace AutenticacaoApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+            private IUserService _userService;
+
+            public UsersController(IUserService userService)
+            {
+                _userService = userService;
+            }
+
+            [AllowAnonymous]
+            [HttpPost("authenticate")]
+
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+                var users = _userService.GetType();
+                return Ok(users);
+            }
+    }
+}
